78694b830c9c4dccd1bf73b55141bfca5d79c0dd
eightfold28 <septialoka@Dharmas-MacBook-Air.local>
2022-06-27 20:55:13 +0700

    save to sharepoint

---
+++ web/lib/usecases.js
@@ -0,0 +1,40 @@
+
+const BACKEND_SERVICE_URL = "http://localhost:8000";
+
+class SaveDiagramToSharePoint {
+
+	constructor() {}
+
+	async execute(filename, code, imageThumb) {
+
+		const formData = new FormData();
+
+		formData.append("filename", filename);
+		formData.append("content", code);
+		formData.append("content_image", imageThumb);
+
+		console.log(formData);
+
+		const response = await fetch(BACKEND_SERVICE_URL + "/save", {
+			method: "POST",
+			body: formData
+		})
+
+		const responseJson = await response.json();
+
+		if (responseJson === "ok") {
+			alert("Diagram berhasil tersimpan.");
+		} else {
+			alert("Terjadi kesalahan saat menyimpan. Silahkan coba beberapa saat lagi.");
+		}
+	}
+}
+
+class GetListDiagramsFromSharePoint {
+
+	constructor() {}
+
+	execute() {
+
+	}
+}
